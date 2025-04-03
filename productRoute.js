1dc0c8ae0c93125f86c804c7635eadfd9023a8c5
meghannulf <mnulfsitter@gmail.com>
2025-04-03 14:03:02 -0700

    meghnulf/backend

---
+++ productRoute.js
@@ -0,0 +1,33 @@
+import express from "express";
+import {
+  listProduct,
+  addProduct,
+  removeProduct,
+  singleProduct,
+} from "../controllers/ProductController.js";
+import upload from "../middleware/multer.js";
+
+const productRouter = express.Router();
+
+//route for adding a product
+productRouter.post(
+  "/add",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
+);
+
+//route for listing all products
+productRouter.get("/list", listProduct);
+
+//route for removing a product
+productRouter.delete("/remove/:id", removeProduct);
+
+//route for single product
+productRouter.get("/single/:id", singleProduct);
+
+export default productRouter;
---
+++ userRoute.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  loginUser,
+  registerUser,
+  adminLogin,
+} from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/login", loginUser);
+userRouter.post("/register", registerUser);
+userRouter.post("/adminlogin", adminLogin);
+
+export default userRouter;
